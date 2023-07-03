77a40560bfed95c9094cd30d41bbcc23dfe1b619
DarkCells <marciomoda18@gmail.com>
2023-07-03 14:21:32 -0300

    Primeiro commit

---
+++ src/show.py
@@ -0,0 +1,22 @@
+from tqdm import tqdm
+import time
+
+
+def menu():
+    pass
+
+def apresentacao():
+    print("Hello World!")
+
+
+def loading():
+    for i in tqdm(range(10)):
+        time.sleep(0.55)       
+
+def animacao():
+    pass
+
+def licensa():
+    pass
+
+
